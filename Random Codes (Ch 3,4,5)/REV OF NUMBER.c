40304f0cac35fa7ebf3c8392090b2f6ed3b42148
Sagar Neupane <neupanesagar2026@gmail.com>
2023-02-04 14:11:04 +0545

    added some more codes

---
+++ Random Codes (Ch 3,4,5)/REV OF NUMBER.c
@@ -0,0 +1,18 @@
+/* WAP TO FIND THE REV OF THE NUMBER */
+#include <stdio.h>
+
+int main(){
+	int a,i,sum=0,rem=0;
+	
+	printf("Enter the value of A: ");
+	scanf("%d",&a);
+	
+	while(a!=0){
+		rem = a % 10;
+		sum=sum*10+rem;
+		a=a/10;
+	}
+	
+	printf("The rev order is: %d",sum);
+	return 0;
+}
